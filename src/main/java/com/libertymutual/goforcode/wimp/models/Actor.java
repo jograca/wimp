228626b81657ff179b7181fbdf208b6be5e1099f
Jon Graca <jon.graca@libertymutual.com>
2017-12-05 15:41:40 -0500

    Initial WIMP App Commit

---
+++ src/main/java/com/libertymutual/goforcode/wimp/models/Actor.java
@@ -0,0 +1,86 @@
+package com.libertymutual.goforcode.wimp.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "actor")
+public class Actor {
+
+	@Id
+	@GeneratedValue(generator = "actor_id_seq", strategy = GenerationType.AUTO)
+	@SequenceGenerator(name = "actor_id_seq", sequenceName = "actor_id_seq")
+	private Long id;
+
+	@Column(length = 75, nullable = false)
+	private String firstName;
+
+	@Column(length = 75, nullable = true)
+	private String lastName;
+
+	@Column(nullable = true)
+	private Long activeSinceYear;
+
+	@Column(nullable = true)
+	private Date birthDate;
+
+	public Actor() {
+
+	}
+
+	public Actor(Long id, String firstName, String lastName, Long activeSinceYear, Date birthDate) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.activeSinceYear = activeSinceYear;
+		this.birthDate = birthDate;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Long getActiveSinceYear() {
+		return activeSinceYear;
+	}
+
+	public void setActiveSinceYear(Long activeSinceYear) {
+		this.activeSinceYear = activeSinceYear;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+}
---
+++ src/main/java/com/libertymutual/goforcode/wimp/models/Movie.java
@@ -0,0 +1,86 @@
+package com.libertymutual.goforcode.wimp.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "movie")
+public class Movie {
+
+	@Id
+	@GeneratedValue(generator = "movie_id_seq", strategy = GenerationType.AUTO)
+	@SequenceGenerator(name = "movie_id_seq", sequenceName = "movie_id_seq")
+	private Long id;
+
+	@Column(length = 300, nullable = false)
+	private String title;
+
+	@Column(nullable = true)
+	private Date releaseDate;
+
+	@Column(nullable = true)
+	private Long budget;
+
+	@Column(length = 500, nullable = false)
+	private String distributor;
+
+	public Movie() {
+
+	}
+
+	public Movie(Long id, String title, Date releaseDate, Long budget, String distributor) {
+		this.id = id;
+		this.title = title;
+		this.releaseDate = releaseDate;
+		this.budget = budget;
+		this.distributor = distributor;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public Long getBudget() {
+		return budget;
+	}
+
+	public void setBudget(Long budget) {
+		this.budget = budget;
+	}
+
+	public String getDistributor() {
+		return distributor;
+	}
+
+	public void setDistributor(String distributor) {
+		this.distributor = distributor;
+	}
+
+}
