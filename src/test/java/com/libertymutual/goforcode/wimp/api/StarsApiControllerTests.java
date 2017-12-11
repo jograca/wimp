b1dfdef641aac61e113b21cb25a7783ea84547a6
Jon Graca <jon.graca@gmail.com>
2017-12-11 16:30:12 -0500

    Completed StartsApiController JUnit Test

--- src/test/java/com/libertymutual/goforcode/wimp/api/StarsApiControllerTests.java
+++ src/test/java/com/libertymutual/goforcode/wimp/api/StarsApiControllerTests.java
@@ -1,8 +1,11 @@
 package com.libertymutual.goforcode.wimp.api;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +13,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import com.libertymutual.goforcode.wimp.models.Actor;
+import com.libertymutual.goforcode.wimp.models.Movie;
 import com.libertymutual.goforcode.wimp.services.ActorRepository;
 import com.libertymutual.goforcode.wimp.services.MovieRepository;
 
@@ -59,21 +63,49 @@ public class StarsApiControllerTests {
 	}
 
 	@Test
-	public void create_associates_a_movie_with_an_actor() {
+	public void create_associates_a_movie_with_an_actor_when_it_is_not_already_listed() {
 		// Arrange
+		Movie movie = new Movie();
+		// Set a blank Array of Movies
+		movie.setActors(new ArrayList<Actor>());
+		Actor actor = new Actor();
+		when(actorRepo.findOne(1L)).thenReturn(actor);
+		when(movieRepo.findOne(2L)).thenReturn(movie);
 
 		// Act
+		Movie actual = controller.create(2L, 1L);
 
 		// Assert
+		assertThat(actual).isSameAs(movie);
+		verify(movieRepo).save(movie);
+		assertThat(movie.getActors()).contains(actor);
+		verify(actorRepo).findOne(1L);
+		verify(movieRepo).findOne(2L);
+
 	}
 
 	@Test
 	public void create_does_not_associate_a_movie_with_an_actor_if_they_are_already_in_it() {
 		// Arrange
+		Actor actor = new Actor();
+		ArrayList<Actor> actors = new ArrayList<Actor>();
+		actors.add(actor);
+		Movie movie = new Movie();
+		movie.setActors(actors);
+
+		when(movieRepo.findOne(1L)).thenReturn(movie);
+		when(actorRepo.findOne(1L)).thenReturn(actor);
 
 		// Act
+		Movie actual = controller.create(1L, 1L);
 
 		// Assert
+		verify(movieRepo).findOne(1L);
+		verify(actorRepo).findOne(1L);
+		assertThat(actual).isSameAs(movie);
+		assertThat(movie.getActors().contains(actor));
+		verify(movieRepo, never()).save(movie);
+		assertThat(movie.getActors()).hasSize(1);
 	}
 
 }
