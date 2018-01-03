132d8aa25fd14920745a6651841a670859f161b8
jon.graca@gmail.com <jon.graca@gmail.com>
2018-01-03 14:40:44 -0500

    Adding CORS Support for Actors

--- src/main/java/com/libertymutual/goforcode/wimp/api/ActorsApiController.java
+++ src/main/java/com/libertymutual/goforcode/wimp/api/ActorsApiController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.libertymutual.goforcode.wimp.services.ActorRepository;
 
 @RestController
 @RequestMapping("/api/actors")
+@CrossOrigin(origins = "*")
 public class ActorsApiController {
 
 	private ActorRepository actorRepo;
