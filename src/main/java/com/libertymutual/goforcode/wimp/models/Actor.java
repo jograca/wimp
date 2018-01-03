e5b1f170c15b8e394972665abca2636ce34b928d
jon.graca@gmail.com <jon.graca@gmail.com>
2018-01-03 10:36:07 -0500

    Updating Date for Actor

--- src/main/java/com/libertymutual/goforcode/wimp/models/Actor.java
+++ src/main/java/com/libertymutual/goforcode/wimp/models/Actor.java
@@ -14,6 +14,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
@@ -41,6 +42,7 @@ public class Actor {
 	@Column(nullable = true)
 	private Long activeSinceYear;
 
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy")
 	@Column(nullable = true)
 	private Date birthDate;
 
